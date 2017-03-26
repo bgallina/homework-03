c49d445d45183d13d9eae570f4cc90490d41f535
bgallina <gallina695@gmail.com>
2017-03-26 17:34:01 +0200

    Fejléc kész (az elején elfelejtettem commitolni, úgyhogy most újra)

--- src/homework-03-functions.R
+++ src/homework-03-functions.R
@@ -1,18 +1,1 @@
-####Functions:
-###5. Feladat
 
-##3.)
-
-get_gender <- function(name) {
-  v = (match(name, comic_characters$name))
-  gender = (if (is.na(comic_characters$gsm[v]) == TRUE) {
-    print(sapply((strsplit(comic_characters$sex[v], " ", 
-                           fixed = T)), "[",1) )
-    
-  } else {
-    print(sapply((strsplit(comic_characters$gsm[v], " ", 
-                           fixed = T)), "[",1))
-    
-  })
-  
-}
